baaa923f2ab165cc2099c7983d927be0ddbec5a4
ta-chibana <tachibana.gmc@gmail.com>
2017-10-12 00:58:57 +0900

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.jsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: path.resolve(__dirname, 'node-modules')
+      }
+    ]
+  },
+  plugins: [
+    // new HtmlWebpackPlugin({
+    //   title: 'React template'
+    // })
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist')
+  }
+}
